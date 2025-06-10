a0319503a46601f56f02ce321b2298f499461bed
Kojo Oppong <princedarf1@gmail.com>
2025-06-10 06:22:12 -0700

    added books

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,13 @@
+
+
+export default function BookCard( {image} ) {
+    return (
+
+            <div>
+                <div><img src={image} alt="Born a Crime" /></div>
+                <h2>Author</h2>
+                <h2>Best Seller</h2>
+            </div>
+
+    );
+}
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,197 +1,38 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import BookCard from '../components/BookCard';
+import Bornacrime from '../assets/images/Bornacrime.jpg';
+import everythingunevahad from '../assets/images/everythingunevahad.jpg';
+import malcomx from '../assets/images/malcomx.jpg'
 
 
-// const MyComponent = () => {
-//   const imageUrl = 'https://example.com/image.jpg'; // Replace with your image URL
-
-//   return (
-//     <div>
-//       <img src={imageUrl} alt="Image Description" />
-//     </div>
-//   );
-// };
-
-// export default MyComponent;/
-
-
-const bornaCrimeUrl = 'https://i.pinimg.com/736x/52/a4/67/52a467fd14af1328cba0e0ca9028f371.jpg'; 
 
 
 export default function Books() {
   return(
     <>
-     <div className='flex grid-cols-4 gap-4 pt-2'>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-    </div>
-     <div className='flex grid-cols-4 gap-4 pt-2'>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-      <div>
-        <div><img src= {bornaCrimeUrl} alt="Born a Crime" /></div>
-        <h2>Author</h2>
-        <h2>Best Seller</h2>
-      </div>
-    </div>
+    <section>
+      <Navbar />
+
+     <div className='grid grid-cols-4 gap-4 m-10'>
+      <BookCard image={Bornacrime} />
+     <BookCard image={everythingunevahad} />
+     <BookCard image={malcomx} />
+     <BookCard image={Bornacrime} />
+     <BookCard image={Bornacrime} />
+     <BookCard image={Bornacrime} />
+     </div>
+
+    <Footer/>
+
+    </section>
+    
     </>
    
     
   );
 }
-
-
-
-// // const booksData = [
-// //   { id: 1, title: 'The Great Gatsby', author: 'F.Scott Fitzegeral', coverImage: './images/gatsby.jpg' },
-// //   { id: 2, title: 'The Great Gatsby', author: 'F.Scott Fitzegeral', coverImage: './images/gatsby.jpg' },
-// //   { id: 3, title: 'The Great Gatsby', author: 'F.Scott Fitzegeral', coverImage: './images/gatsby.jpg' }
-// // ];
-
-
-// // export default function Books() {
-// //   return (
-//     // <div>
-//     //   <h1 className='font-bold text-4xl text-center border'>Books Collection</h1>
-//     //   <div>
-//     //     <div>
-//     //       <button>Preview</button>
-//     //       <span>Add book</span>
-//     //     </div>
-//     //   </div>
-
-//     // </div>
-
-// const books = [
-//   {
-//     title: 'The Eye of the World',
-//     image: 'https://example.com/eye-of-world.jpg',
-//     buttonText: 'Preview Only'
-//   },
-//   {
-//     title: 'Grumpy Darling',
-//     image: 'https://example.com/grumpy-darling.jpg',
-//     buttonText: 'Locate'
-//   },
-//   {
-//     title: 'A Game of Thrones',
-//     image: 'https://example.com/game-of-thrones.jpg',
-//     buttonText: 'Preview Only'
-//   },
-//   {
-//     title: 'Fifty Shades of Grey',
-//     image: 'https://example.com/fifty-shades.jpg',
-//     buttonText: 'Preview Only'
-//   },
-//   {
-//     title: 'We Were Never Meant to Be',
-//     image: 'https://example.com/we-were-never.jpg',
-//     buttonText: 'Locate'
-//   },
-//   {
-//     title: "I Don't Love You Anymore",
-//     image: 'https://example.com/dont-love-you.jpg',
-//     buttonText: 'Locate'
-//   }
-// ];
-
-// export default function Books() {
-// return (
-//    +
-//   );
-// }
-// //   const carouselRef = useRef(null);
-
-// //   const scroll = (direction) => {
-// //     if (carouselRef.current) {
-// //       carouselRef.current.scrollBy({
-// //         left: direction * 200,
-// //         behavior: 'smooth'
-// //       });
-// //     }
-// //   };
-
-  
 
 
 
